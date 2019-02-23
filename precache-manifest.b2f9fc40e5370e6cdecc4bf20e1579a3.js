d52f3e907ab675ab208f4288bdd832f5b57079c6
Bo Hu <boh@vmware.com>
2019-02-22 17:44:02 -0800

    Updates

---
+++ precache-manifest.b2f9fc40e5370e6cdecc4bf20e1579a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "ebd703577f638af7a687",
+    "url": "/contactmanager/static/js/main.ebd70357.chunk.js"
+  },
+  {
+    "revision": "9c4b17b992cdb72c091f",
+    "url": "/contactmanager/static/js/2.9c4b17b9.chunk.js"
+  },
+  {
+    "revision": "ebd703577f638af7a687",
+    "url": "/contactmanager/static/css/main.ac4bdfb3.chunk.css"
+  },
+  {
+    "revision": "9c4b17b992cdb72c091f",
+    "url": "/contactmanager/static/css/2.cf569b9b.chunk.css"
+  },
+  {
+    "revision": "47e8568e12d23b8b21f06f11473608d5",
+    "url": "/contactmanager/index.html"
+  }
+];
